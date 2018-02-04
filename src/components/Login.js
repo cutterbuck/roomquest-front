f00729f00ebb6dc7feea262ef9cb4ea68960e055
Jake <jakemacnaughton@gmail.com>
2018-02-04 12:14:27 -0500

    formatted the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,7 @@ import { connect }  from 'react-redux';
 class Login extends React.Component {
 
   responseFacebook = (response) => {
+    console.log('------------------------------------');
     console.log('TOTAL RESPONSE:', response);
     console.log('friends that use this app:', response.friends.data)
     const history = this.props.history
@@ -18,15 +19,30 @@ class Login extends React.Component {
   render () {
     return (
       <div>
-          <FacebookLogin socialId='1824814727815715'    //{ENV["FACEBOOK_APP_ID"]}
-                         className="facebook-login-button"
-                         language="en_US"
-                         scope="public_profile,email,user_friends"
-                         responseHandler={this.responseFacebook}
-                         xfbml={true}
-                         fields="id,email,name,picture,friends"
-                         version="v2.12"
-                         buttonText="Continue With Facebook" />
+        <div className="background">
+        </div>
+        <div className="login-style">
+          <div className="ui middle aligned center aligned grid">
+            <div className="column">
+              <div className="ui gray image header">
+                <h1>RoomQuest</h1>
+                <div className="title">
+                  Find your new home!<br></br>
+                  Search for your next roommate!
+                </div>
+                <FacebookLogin socialId='1824814727815715'    //{ENV["FACEBOOK_APP_ID"]}
+                               className="facebook-login-button"
+                               language="en_US"
+                               scope="public_profile,email,user_friends"
+                               responseHandler={this.responseFacebook}
+                               xfbml={true}
+                               fields="id,email,name,picture,friends"
+                               version="v2.12"
+                               buttonText="Login With Facebook" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
