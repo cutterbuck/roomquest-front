dad020841ab5893baae152f0d99357450ea2a546
Jake <jakemacnaughton@gmail.com>
2018-02-04 17:11:12 -0500

    worked on homepage cards

--- src/components/RoomQuestContainer.js
+++ src/components/RoomQuestContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NavBar from './NavBar';
 import * as actions from '../actions/index';
+import { Link } from 'react-router-dom';
 
 class RoomQuestContainer extends React.Component {
   render() {
@@ -17,20 +18,24 @@ class RoomQuestContainer extends React.Component {
           <div className="ui two column stackable grid">
             {loggedIn ? (<div className="first header"><h1 className="welcome">Welcome to RoomQuest, {this.props.currentUser.name.split(" ")[0]}</h1></div>) : null}
             <div className="column">
-              <div className="card">
-                <div className="content">
-                  <div className="header"><h2>Find a Room</h2></div>
-                  <div className="description">Looking for a new place to live?<br></br>Moving to a new city?<br></br><br></br>See which friends and acquaintances have available rooms.</div>
+              <Link to={`/vacancy-form`}>
+                <div className="card">
+                  <div className="content">
+                    <div className="header"><h2>Find a Room</h2></div>
+                    <div className="description">Looking for a new place to live?<br></br>Moving to a new city?<br></br><br></br>See which friends and acquaintances have available rooms.</div>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
             <div className="column">
-              <div className="card">
-                <div className="content">
-                  <div className="header"><h2>Find a Roommate</h2></div>
-                  <div className="description">Is one of your roommates moving away?<br></br>Have an empty room?<br></br><br></br>Post a vacancy to see which friends and acquaintances might be interested.</div>
+              <Link to={`/search`}>
+                <div className="card">
+                  <div className="content">
+                    <div className="header"><h2>Find a Roommate</h2></div>
+                    <div className="description">Is one of your roommates moving away?<br></br>Have an empty room?<br></br><br></br>Post a vacancy to see which friends and acquaintances might be interested.</div>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           </div>
         </div>
