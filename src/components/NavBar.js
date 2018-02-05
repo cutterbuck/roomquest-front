ae580fe6f228a6d6a51cb750552277078244ee4e
Jake <jakemacnaughton@gmail.com>
2018-02-05 10:49:02 -0500

    finished form html

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/index';
 import Logout from './Logout';
+import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
+
+
 
 
   render() {
     return (
       <div className="ui inverted black menu fixed">
         <div className="right menu">
+          <div className="right aligned item">
+            <Link to="/home">
+              <div className="content">Home</div>
+            </Link>
+          </div>
           <Logout />
         </div>
       </div>
